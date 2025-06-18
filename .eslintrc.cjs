5ce50f485f424fa5d7aee56d24282e42a9132824
thakicloud-yunjaepark <yunjae.park@thakicloud.co.kr>
2025-06-18 17:48:38 +0900

    fix: Add ESLint configuration and fix workflow

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true, jest: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'k8s'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'react'],
+  rules: {
+    'react/prop-types': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}; 
