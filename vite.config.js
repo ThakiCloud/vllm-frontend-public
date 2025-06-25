9077f66508891a2f14bb2344a893789bb0685eab
thakicloud-yunjaepark <yunjae.park@thakicloud.co.kr>
2025-06-25 14:51:57 +0900

    Fix backend url issue

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/standardized_output': 'http://localhost:8000',
-      '/raw_input': 'http://localhost:8000',
+      '/standardized_output': 'http://vllm-backend:8000',
+      '/raw_input': 'http://vllm-backend:8000',
     },
   },
   test: {
